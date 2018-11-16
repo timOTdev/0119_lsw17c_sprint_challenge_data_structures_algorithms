b3626530113600b8fe333edbef66586e2c53a845
Timothy Hoang <timh1203@gmail.com>
2018-11-16 11:30:42 -0500

    Add breadth first search

--- data_structures/ex1/binary_search_tree.py
+++ data_structures/ex1/binary_search_tree.py
@@ -1,46 +1,54 @@
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def depth_first_for_each(self, cb):
-    pass    
+    def depth_first_for_each(self, cb):
+        pass
 
-  def breadth_first_for_each(self, cb):
-    pass
+    def breadth_first_for_each(self, cb):
+        queue = [self]  # Add root node to queue
 
-  def insert(self, value):
-    new_tree = BinarySearchTree(value)
-    if (value < self.value):
-      if not self.left:
-        self.left = new_tree
-      else:
-        self.left.insert(value)
-    elif value >= self.value:
-      if not self.right:
-        self.right = new_tree
-      else:
-        self.right.insert(value)
+        while queue:  # while there's a node in the queue
+            discovered_node = queue.pop(0)
+            if discovered_node.left:  # if there's a left node, add to queue
+                queue.append(discovered_node.left)
+            if discovered_node.right:  # if there's a right node, add to queue
+                queue.append(discovered_node.right)
+            cb(discovered_node.value)  # run the cb on that node
 
-  def contains(self, target):
-    if self.value == target:
-      return True
-    if self.left:
-      if self.left.contains(target):
-        return True
-    if self.right:
-      if self.right.contains(target):
-        return True
-    return False
+    def insert(self, value):
+        new_tree = BinarySearchTree(value)
+        if value < self.value:
+            if not self.left:
+                self.left = new_tree
+            else:
+                self.left.insert(value)
+        elif value >= self.value:
+            if not self.right:
+                self.right = new_tree
+            else:
+                self.right.insert(value)
 
-  def get_max(self):
-    if not self:
-      return None
-    max_value = self.value
-    current = self
-    while current:
-      if current.value > max_value:
-        max_value = current.value
-      current = current.right
-    return max_value
+    def contains(self, target):
+        if self.value == target:
+            return True
+        if self.left:
+            if self.left.contains(target):
+                return True
+        if self.right:
+            if self.right.contains(target):
+                return True
+        return False
+
+    def get_max(self):
+        if not self:
+            return None
+        max_value = self.value
+        current = self
+        while current:
+            if current.value > max_value:
+                max_value = current.value
+            current = current.right
+        return max_value
